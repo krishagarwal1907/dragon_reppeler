93306f71b02a15898c2d234239db60f7fe4f5d01
Krish Agarwal <116301754+krishagarwal1907@users.noreply.github.com>
2023-06-16 09:52:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const weapons = [
 		name: "sword",
 		power: 100
 	}
-]
+];
 
 const monsters = [
   {
@@ -52,7 +52,7 @@ const monsters = [
     level: 20,
     health: 300
   }
-]
+];
 
 const locations = [
     {
@@ -78,6 +78,30 @@ const locations = [
 		"button text": ["Attack", "Dodge", "Run"],
 		"button functions": [attack, dodge, goTown],
 		text: "You are fighting a monster."
+	},
+	{
+		name: "kill monster",
+		"button text": ["Go to town square", "Go to town square", "Go to town square"],
+		"button functions": [goTown, goTown, easterEgg],
+		text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+	},
+	{
+		name: "lose",
+		"button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+		"button functions": [restart, restart, restart],
+		text: "You die. ☠️"
+	},
+	{
+		name: "win",
+		"button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+		"button functions": [restart, restart, restart],
+		text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
+    },
+	{
+		name: "easter egg",
+		"button text": ["2", "8", "Go to town square?"],
+		"button functions": [pickTwo, pickEight, goTown],
+		text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
 	}
 ]
 
@@ -181,4 +205,104 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     
- }
+    if (isMonsterHit()) {
+        health -= getMonsterAttackValue(monsters[fighting].level);
+    } else {
+		text.innerText += " You miss.";
+	}
+    
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+	healthText.innerText = health;
+	monsterHealthText.innerText = monsterHealth;   
+	if (health <= 0) {
+		lose();
+	} else if (monsterHealth <= 0) {
+		fighting === 2 ? winGame() : defeatMonster();
+	}
+
+	if (Math.random() <= .1 && inventory.length !== 1) {
+        text.innerText += " Your " + inventory.pop() + " breaks.";
+        currentWeapon--;
+	}
+}
+
+function getMonsterAttackValue(level) {
+    let hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return hit;
+}
+
+function isMonsterHit() {
+	return Math.random() > .2 || health < 20;
+}
+
+
+function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7)
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+	xpText.innerText = xp;
+    update(locations[4]);
+}
+
+function lose() {
+    update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+	xp = 0;
+	health = 100;
+	gold = 50;
+	currentWeapon = 0;
+	inventory = ["stick"];
+	goldText.innerText = gold;
+	healthText.innerText = health;
+	xpText.innerText = xp;
+	goTown();
+}
+
+function easterEgg() {
+	update(locations[7]);
+}
+
+function pickTwo() {
+ pick(2);
+}
+
+function pickEight() {
+ pick(8);
+}
+
+function pick(guess) {
+    let numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + "\n";
+    }
+
+    if (numbers.indexOf(guess) !== -1) {
+        text.innerText += "Right! You win 20 gold!"
+        gold += 20;
+        goldText.innerText = gold;
+    } else {
+        text.innerText += "Wrong! You lose 10 health!"
+        health -= 10;
+        healthText.innerText = health
+        if (health <= 0) {
+          lose();
+        }
+    }
+}
